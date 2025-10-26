5f351bf072198b1d99adc8d1bf800b9f5b426d1b
samarjitsutradhar36246-dev <samarjitsutradhar36246@gmail.com>
2025-10-26 19:30:58 +0530

    Creating a Calculator

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+function App() {
+  return (
+    <div id="calculator">
+      <input type="text" />
+      <div id="container">
+        <button>C</button>
+      </div>
+    </div>
+  );
+}
+export default App;
